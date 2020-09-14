8712227e0a872487314743bc8e61dfde45884fdb
acarter881 <60497471+acarter881@users.noreply.github.com>
2020-09-13 23:23:57 -0500

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,11 +1,19 @@
-# Making the simplest impossible math problem
+# Making the simplest impossible math problem (with error handling)
 
 def collatz(number):
     if number % 2 == 0:
         return number // 2
     return 3 * number + 1
 
-user_number = int(input('Please input a positive integer: \n'))
+
+while 1:
+    try:
+        user_number = int(input('Please input a positive integer: \n'))
+        if user_number < 1:
+            continue
+        break
+    except ValueError:
+        continue
 
 while 1:
     if collatz(user_number) == 1:
